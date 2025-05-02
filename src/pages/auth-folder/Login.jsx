e4af5238db97d77f4f92911919042b7e87f2859b
nasemul1 <nasemul.me@gmail.com>
2025-05-02 15:17:41 +0600

    updated login and register page

--- src/pages/auth-folder/Login.jsx
+++ src/pages/auth-folder/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import side from '../../assets/Others/login.webp'
+import ist_logo from '../../assets/IST_logo.gif'
+import pcist_logo from '../../../public/vite.svg'
 
 const Login = () => {
 
@@ -12,15 +14,16 @@ const Login = () => {
 		        <div id="left-side" className='w-3/6 hidden md:block bg-cover' style={{backgroundImage: `url(${side})`}}></div>
 		        <div id="right-side" className='p-6 w-full md:w-3/6 text-black bg-white md:rounded-r-lg flex flex-col justify-center items-center'>
 		          <form className='w-full flex flex-col items-center justify-center'>
-		            <div className='w-3/4'>
-		              <p className='text-left'>Hello !</p>
-		              <p className='text-[#FF6900] font-bold'>Greetings</p>
+		            <div className='w-3/4 flex justify-center gap-5'>
+		              <img src={ist_logo} className='h-[64px] w-[64px]' />
+		              <div className='w-[2px] h-full bg-gray-500'></div>
+		              <img src={pcist_logo} className='h-[64px] w-[64px]' />
 		            </div>
 		            <p className='m-5 align-center text-center'><span className='text-[#FF6900] font-semibold'>Login</span> Your Account</p>
-		            <input placeholder='Username' type="text" id='user' value='' className='mb-2 p-2 w-3/4 bg-inherit border-b-2 border-[#020618] rounded-sm outline-none' required/><br />
+		            <input placeholder='Class Roll' type="number" id='roll' value='' className='mb-2 p-2 w-3/4 bg-inherit border-b-2 border-[#020618] rounded-sm outline-none' required/><br />
 		            <input placeholder='Password' type="password" name="pass" id="pass" value='' onChange={e => setPass(e.target.value)} className='mb-5 p-2 w-3/4 bg-inherit border-b-2 border-[#020618] rounded-sm outline-none' required/><br />
 		            <button type="submit" className='cursor-pointer pl-4 pr-4 pt-2 pb-2 w-3/4 text-white font-bold bg-[#020618] hover:bg-[#020618]/75 border-0 rounded-sm'>Login</button>
-		            <button onClick={()=>navigate(-1)} className='cursor-pointer mt-2 pl-4 pr-4 pt-2 pb-2 w-3/4 text-red-400 font-bold bg-gray-200 hover:bg-gray-200/75  border-0 rounded-sm'>Back</button>
+		            <button onClick={()=>navigate(-1)} className='cursor-pointer mt-2 pl-4 pr-4 pt-2 pb-2 w-3/4 text-[#FF6900] font-bold bg-gray-200 hover:bg-gray-200/75  border-0 rounded-sm'>Back</button>
 		            <div className='mt-6'>
 		              <Link to='/' className='mt-6 text-[#FF6900]'>Home</Link>
 		              <span className='ml-2 mr-2'>|</span>
